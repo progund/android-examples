7845225ec8d22956b038bb7ab57ca9467fb4befd
Henrik Sandklef <hesa@sandklef.com>
2018-11-26 14:32:19 +0100

    first rev

---
+++ common-data/members.js
@@ -0,0 +1,15 @@
+[
+    {
+        "name": "Henrik Sandklef",
+        "email": "nobrain@juneday.se"
+    },
+    {
+        "name": "Rikard Fröberg",
+        "email": "gobrain@juneday.se"
+    },
+    {
+        "name": "Smarty Pants",
+        "email": "smrt@juneday.se"
+    }
+]
+    
